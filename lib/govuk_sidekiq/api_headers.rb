4ef4d0e6efcfe622ebe28e73299e3fdd582d5ac9
Steve Laing <steve.laing@digital.cabinet-office.gov.uk>
2016-06-03 14:41:52 +0000

    Recycle authenticated user uid header...
    The GdsApi::GovukHeaders header x_govuk_authenticated_user is used to identify
    the initiating user for an event, this should be made available to the job
    and repopulate this header for downstream requests.

--- lib/govuk_sidekiq/api_headers.rb
+++ lib/govuk_sidekiq/api_headers.rb
@@ -8,8 +8,15 @@ module GovukSidekiq
     # https://github.com/mperham/sidekiq/wiki/Middleware#client-side-middleware
     class ClientMiddleware
       def call(worker_class, job, queue, redis_pool)
-        job["args"] << { request_id: GdsApi::GovukHeaders.headers[:govuk_request_id] }
+        job["args"] << header_arguments
         yield
+      end
+
+      def header_arguments
+        {
+          authenticated_user: GdsApi::GovukHeaders.headers[:x_govuk_authenticated_user],
+          request_id: GdsApi::GovukHeaders.headers[:govuk_request_id],
+        }
       end
     end
 
@@ -20,10 +27,12 @@ module GovukSidekiq
       def call(worker, message, queue)
         last_arg = message["args"].last
 
-        if last_arg.is_a?(Hash) && last_arg.keys == ["request_id"]
+        if last_arg.is_a?(Hash) && last_arg.keys.include?("request_id")
           message["args"].pop
           request_id = last_arg["request_id"]
+          authenticated_user = last_arg["authenticated_user"]
           GdsApi::GovukHeaders.set_header(:govuk_request_id, request_id)
+          GdsApi::GovukHeaders.set_header(:x_govuk_authenticated_user, authenticated_user)
         end
 
         yield
