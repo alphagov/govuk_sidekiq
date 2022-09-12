20adc4464eb64e3f632b5bce7b08e972b23255fe
JonathanHallam <jonathan.hallam@digital.cabinet-office.gov.uk>
2022-09-12 14:37:34 +0100

    Configure logger for stdout
    This implements some snowflakey GOV.UK behaviour around stderr/out that's explained in a comment

--- lib/govuk_sidekiq/sidekiq_initializer.rb
+++ lib/govuk_sidekiq/sidekiq_initializer.rb
@@ -14,6 +14,15 @@ module GovukSidekiq
       Sidekiq.configure_server do |config|
         config.log_formatter = Sidekiq::Logger::Formatters::JSON.new if ENV["GOVUK_SIDEKIQ_JSON_LOGGING"]
 
+        # $real_stdout is defined by govuk_app_config and is used to point to
+        # STDOUT as that redirects $stdout to actually be $stderr.
+        # When govuk_app_config does this we need to use $real_stdout to output logs to STDOUT.
+        # https://github.com/alphagov/govuk_app_config/blob/08fd9cf6a848615261b3cef021e34490ed72ee55/lib/govuk_app_config/govuk_logging.rb#L18-L24
+
+        # rubocop:disable Style/GlobalVars
+        config.logger = Sidekiq::Logger.new($real_stdout) if defined?($real_stdout)
+        # rubocop:enable Style/GlobalVars
+
         config.redis = redis_config
 
         config.server_middleware do |chain|
