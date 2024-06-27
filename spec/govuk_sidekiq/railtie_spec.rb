938deb2dea3d9141b247db9865a5484f85dd8de0
Bruce Bolt <bruce.bolt@digital.cabinet-office.gov.uk>
2024-06-27 10:45:34 +0100

    Auto-correct rubocop failures

--- spec/govuk_sidekiq/railtie_spec.rb
+++ spec/govuk_sidekiq/railtie_spec.rb
@@ -3,7 +3,7 @@ require "rails"
 require "govuk_sidekiq/railtie"
 
 RSpec.describe GovukSidekiq::Railtie do
-  let(:app) { instance_double("Rails::Application", root: Pathname.new("rails/app")) }
+  let(:app) { instance_double(Rails::Application, root: Pathname.new("rails/app")) }
   let(:default_app_name) { "app" }
   let(:default_redis_configuration) { { url: "redis://127.0.0.1:6379" } }
 
