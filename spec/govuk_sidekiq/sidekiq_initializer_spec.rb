1464de92f3bfc35b649b308b9a57bae3942fb5d5
Bruce Bolt <bruce.bolt@digital.cabinet-office.gov.uk>
2024-06-27 10:48:07 +0100

    Add expectation to test
    This test contains no expectation or assertion. The intention may have
    been that no error is raised, so explicitly adding an expectation for
    that.

--- spec/govuk_sidekiq/sidekiq_initializer_spec.rb
+++ spec/govuk_sidekiq/sidekiq_initializer_spec.rb
@@ -1,7 +1,9 @@
 require "govuk_sidekiq/sidekiq_initializer"
 
 RSpec.describe GovukSidekiq::SidekiqInitializer do
-  it "doesn't explode when called" do
-    described_class.setup_sidekiq("govuk_app_name", { url: "redis://redis" })
+  it "doesn't error when called" do
+    expect {
+      described_class.setup_sidekiq("govuk_app_name", { url: "redis://redis" })
+    }.not_to raise_error
   end
 end
