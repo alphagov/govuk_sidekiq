ed29d1135fe03c2ac208257110520f43665887b3
Kevin Dew <kevindew@me.com>
2024-08-19 21:32:40 +0100

    Don't verify TLS
    This is needed when running a Heroku Redis instance other than mini
    where you are only given a rediss:// URL and self signed certificate.
    
    This turns off verification of the certificate.
    
    Sidekiq fails to run without this [1].
    
    This is a quick proof of concept so I can link to this branch for a
    heroku example, as I couldn't work out how to disable the initializer in
    the Railstie so I could run the below code in my app directly.
    
    [1]: https://github.com/sidekiq/sidekiq/wiki/Heroku#redis

--- lib/govuk_sidekiq/railtie.rb
+++ lib/govuk_sidekiq/railtie.rb
@@ -4,7 +4,7 @@ module GovukSidekiq
   class Railtie < Rails::Railtie
     initializer "govuk_sidekiq.initialize_sidekiq" do
       SidekiqInitializer.setup_sidekiq(
-        { url: ENV.fetch("REDIS_URL", "redis://127.0.0.1:6379") },
+        { url: ENV.fetch("REDIS_URL", "redis://127.0.0.1:6379"), ssl_params: { verify_mode: OpenSSL::SSL::VERIFY_NONE } },
       )
     end
 
